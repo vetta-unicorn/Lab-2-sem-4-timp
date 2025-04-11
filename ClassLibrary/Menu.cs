39aafbbb76a3dbb8f26c5be24f6e8e08078d8456
vetta_unicorn <lisalantuh@gmail.com>
2025-04-11 16:50:14 +0700

    Made a non-binary tree

---
+++ ClassLibrary/Menu.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Menu
+    {
+        private List<Tree> menu;
+        private ProcessFile fileHandler;
+
+        public Menu(string filePath)
+        {
+            menu = new List<Tree>();
+            fileHandler = new ProcessFile(filePath);
+        }
+
+        public int FindLastRoot(int level)
+        {
+            int len = menu.Count;
+            for (int i = len - 1; i >= 0; i--)
+            {
+                if (menu[i].root.GetLevel() == (level - 1))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        public void SetMenu()
+        {
+            int i = 0;
+            string[] lines = fileHandler.GetFiles();
+            Item prev = new Item();
+            Tree prev_tree = new Tree();
+
+            while (i < lines.Length)
+            {
+                int level = fileHandler.GetLevel(lines[i]);
+                string name = fileHandler.GetPointName(lines[i]);
+                string methName = fileHandler.GetMethodName(lines[i]);
+                Item item = new Item(level, name, methName);
+                Tree tree = new Tree(item);
+
+                if (level == 0)
+                {
+                    menu.Add(tree);
+                }
+
+                else if (prev.GetLevel() < level)
+                {
+                    menu.Last().addChild(tree);
+                }
+
+                else if (prev.GetLevel() == level && level != 0)
+                {
+                    try
+                    {
+                        menu[FindLastRoot(level)].addChild(tree);
+                    }
+                    catch 
+                    {
+                        Console.WriteLine("No root found!");
+                    }
+                }
+
+                prev = item;
+                prev_tree = tree;
+                i++;
+            }
+        }
+    }
+}
---
+++ Lab-2-sem-4-timp/File.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab_2_sem_4_timp
+{
+    public class ProcessFile
+    {
+        public ProcessFile() { }
+
+        public string[] GetFiles(string filePath)
+        {
+            string[] lines = { "" };
+            try
+            {
+                lines = File.ReadAllLines(filePath);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла ошибка: {ex.Message}");
+            }
+            return lines;
+        }
+
+        public string[] SplitStrings(string line)
+        {
+            // Разделяем строку по пробелам
+            string[] parts = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            return parts;
+        }
+
+        public int GetLevel(string line)
+        {
+            string[] parts = SplitStrings(line);
+            int number = int.Parse(parts[0]);
+            return number;
+        }
+
+        public string GetPointName(string line)
+        {
+            string[] parts = SplitStrings(line);
+            string name = parts[1]; 
+            return name;
+        }
+
+        public string GetMethodName(string line)
+        {
+            string[] parts = SplitStrings(line);
+            string methName = "";
+            if (parts[2] != null)
+            {
+                methName = parts[2];
+            }
+            return methName;
+        }
+    }
+}
